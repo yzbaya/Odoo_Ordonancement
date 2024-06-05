c76e784b81b09b9e449afe5b61f3e56f3bb7dab2
Ons Yazidi <101043195+yzbaya@users.noreply.github.com>
2024-06-05 18:56:46 +0100

    Add files via upload

--- models.py
+++ models.py
@@ -1,19 +1,18 @@
-# -*- coding: utf-8 -*-
+import os
+import logging
+import joblib
+from odoo import models, api,fields, _
+# Assurez-vous d'importer _ pour la traduction
+_logger = logging.getLogger(__name__)
 
-# from odoo import models, fields, api
-
-
-# class my_sale(models.Model):
-#     _name = 'my_sale.my_sale'
-#     _description = 'my_sale.my_sale'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
+class CustomMrpProduction(models.Model):
+    _inherit = 'mrp.production'
+    sale_order_id = fields.Many2one('sale.order',string="Référence de commande")
+    # def write(self, vals):
+    #     res = super(CustomMrpProduction, self).write(vals)
+    #     if 'state' in vals:
+    #         for mo in self:
+    #             related_order = self.env['sale.order'].search([('name', '=', mo.name)], limit=1)
+    #             if related_order:
+    #                 related_order._compute_status_commande()
+    #     return res

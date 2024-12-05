88ff7c24c046811ccccf0c9a628c02f1a8c74900
Ons Yazidi <101043195+yzbaya@users.noreply.github.com>
2024-12-05 01:31:57 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,18 +1,20 @@
-import os
-import logging
-import joblib
-from odoo import models, api,fields, _
-# Assurez-vous d'importer _ pour la traduction
-_logger = logging.getLogger(__name__)
+# -*- coding: utf-8 -*-
 
-class CustomMrpProduction(models.Model):
+# my_fabrication.py
+from odoo import models, fields
+
+class MrpProduction(models.Model):
     _inherit = 'mrp.production'
+
     sale_order_id = fields.Many2one('sale.order',string="Référence de commande")
-    # def write(self, vals):
-    #     res = super(CustomMrpProduction, self).write(vals)
-    #     if 'state' in vals:
-    #         for mo in self:
-    #             related_order = self.env['sale.order'].search([('name', '=', mo.name)], limit=1)
-    #             if related_order:
-    #                 related_order._compute_status_commande()
-    #     return res
+
+
+class MrpWorkorder(models.Model):
+    _inherit = 'mrp.workorder'
+
+    def button_finish(self, elapsed_seconds=None):
+        for order in self:
+            if elapsed_seconds:
+                order.duration = elapsed_seconds / 3600.0  # Assuming duration is stored in hours
+            # Proceed with the original finish process
+            super(MrpWorkorder, order).button_finish()
